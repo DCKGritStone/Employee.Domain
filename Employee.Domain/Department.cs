8070d03dc86f1af251cfedc12e13dcf44f4eb6ca
Drishtanth Kuppadan <drishtanth.kuppadan@planckepoch.com>
2024-04-09 15:34:01 +0530

    Add project files.

---
+++ Employee.Domain/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Domain
+{
+    public class Department
+    {
+       [Key] public string? Designation { get; set; }
+        public string? DeptId { get; set; }
+
+    }
+}
---
+++ Employee.Domain/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Employee.Domain
+{
+    public class Employee
+    {
+        [Key] public string? Name { get; set; }
+        public string? Designation { get; set; }
+        [ForeignKey("Designation")]
+
+        public double Salary { get; set; }
+
+    }
+}
